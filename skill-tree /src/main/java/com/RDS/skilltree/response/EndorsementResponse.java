868341745e007ed386c4573499db28f543b59d38
vikhyat <vikhyat.bhatnagar@jumbotail.com>
2023-08-06 08:59:36 +0530

    Updated tests response for POST, PATCH call

--- skill-tree /src/main/java/com/RDS/skilltree/response/EndorsementResponse.java
+++ skill-tree /src/main/java/com/RDS/skilltree/response/EndorsementResponse.java
@@ -2,9 +2,13 @@ package com.RDS.skilltree.response;
 
 import com.RDS.skilltree.enums.EndorsementType;
 import com.RDS.skilltree.enums.Status;
+import lombok.Builder;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
+@Builder
 public class EndorsementResponse {
 
     private static class EndorserList{
--- skill-tree /src/test/java/com/RDS/skilltree/EndorsementsIntegrationTests.java
+++ skill-tree /src/test/java/com/RDS/skilltree/EndorsementsIntegrationTests.java
@@ -71,7 +71,8 @@ public class EndorsementsIntegrationTests {
     @DisplayName("Fetch all the endorsements given endorsement status")
     public void testAPIReturnsEndorsementsGivenStatus(){
         Response response = given()
-                .get("/endorsements?status=PENDING");
+                        .queryParam("status", "PENDING")
+                        .get("/endorsements");
 
         response.then()
                 .statusCode(200)
@@ -79,7 +80,7 @@ public class EndorsementsIntegrationTests {
                 .body("data", hasSize(1))
                 .body("data[0].endorseeId",equalTo("user-1"))
                 .body("data[0].skillName",equalTo("Java"))
-                .body("data[0].status", anyOf(equalTo("APPROVED"), equalTo("REJECTED"), equalTo("PENDING")))
+                .body("data[0].status", equalTo("PENDING"))
                 .body("data[0].endorsementType",anyOf(equalTo("POSITIVE"), equalTo("NEGATIVE")))
                 .body("data[0].endorsersList", hasSize(1))
                 .body("data[0].endorserList[0].endorserId",equalTo("user-2"))
@@ -121,25 +122,16 @@ public class EndorsementsIntegrationTests {
                 .post("/endorsements");
         response.then()
                 .statusCode(200)
+                .contentType("application/json")
                 .body("code", equalTo(200))
-                .body("message", equalTo("Created endorsement sucessfully"));
-
-
-    }
-
-    @Test
-    @Disabled
-    @DisplayName("Return 200 on endorsement updation")
-    public void testAPIReturns200_OnEndorsementsUpdation(){
-        String endorsementId = "e-1";
-        Response response = given()
-                .pathParam("endorsementId", endorsementId)
-                .patch("/endorsements");
-
-        response.then()
-                .statusCode(200)
-                .body("code",equalTo(200))
-                .body("message", equalTo("Updated endorsement sucessfully"));
+                .body("data.endorseeId", equalTo("user-1"))
+                .body("data.skillName", equalTo("Java"))
+                .body("data.status",anyOf(equalTo("APPROVED"),equalTo("PENDING"),equalTo("REJECTED")))
+                .body("data.endorsementType",anyOf(equalTo("POSITIVE"),equalTo("NEGATIVE")))
+                .body("data.endorsersList",hasSize(1))
+                .body("data.endorsersList[0].endorserId",equalTo("user-2"))
+                .body("data.endorsersList[0].description",isA(String.class))
+                .body("data.endorsersList[0].userType",anyOf(equalTo("SUPER_USER"),equalTo("MAVEN"),equalTo("USER")));
     }
 
     @Test
@@ -168,18 +160,18 @@ public class EndorsementsIntegrationTests {
 
         /*
         {
-  "endorseeId": "user-1",
-  "skillName": "Java",
-  "status": "APPROVED",
-  "endorsementType": "POSITIVE",
-  "endorsersList": [
-    {
-      "endorserId": "user-2",
-      "description": "string",
-      "userType": "NORMAL_USER"
-    }
-  ]
-}
+          "endorseeId": "user-1",
+          "skillName": "Java",
+          "status": "APPROVED",
+          "endorsementType": "POSITIVE",
+          "endorsersList": [
+                {
+                  "endorserId": "user-2",
+                  "description": "string",
+                  "userType": "NORMAL_USER"
+                }
+              ]
+        }
          */
 
         response.then()
@@ -208,7 +200,7 @@ public class EndorsementsIntegrationTests {
 
         response.then()
                 .statusCode(400)
-                .body("code",equalTo(404))
+                .body("code",equalTo(400))
                 .body("message", equalTo("Invalid endorsementId passed"));
     }
 
