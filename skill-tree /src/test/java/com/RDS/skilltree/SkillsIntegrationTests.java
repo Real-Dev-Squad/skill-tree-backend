993bddcc19be53ecf243e64ac0f61451038c5bc8
vikhyat187 <vikhyatbhatnagarh@gmail.com>
2023-12-02 17:50:27 +0530

    Added the dependency for test containers and logging format for console logs

--- skill-tree /src/test/java/com/RDS/skilltree/SkillsIntegrationTests.java
+++ skill-tree /src/test/java/com/RDS/skilltree/SkillsIntegrationTests.java
@@ -2,23 +2,44 @@ package com.RDS.skilltree;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT )
 public class SkillsIntegrationTests {
 
-    @Before
-    public void setup() {
+    private static MySQLContainer mysqlContainer;
+
+    static {
+        mysqlContainer = new MySQLContainer("mysql:latest")
+                .withDatabaseName("skilltree-test")
+                .withUsername("root")
+                .withPassword("password");
+        mysqlContainer.start();
         RestAssured.baseURI = "http://localhost:8080";
     }
 
+    @DynamicPropertySource
+    static void configureTestProperties(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url", ()-> mysqlContainer.getJdbcUrl());
+        System.out.println("The spring datasource url is " + mysqlContainer.getJdbcUrl());
+        registry.add("spring.datasource.username", () -> mysqlContainer.getUsername());
+        registry.add("spring.datasource.password", () -> mysqlContainer.getPassword());
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create");
+    }
+
     @Test
-    @Disabled
     @DisplayName("Return all skills")
     public void testAPIReturnsAllSkills() {
 
@@ -51,7 +72,6 @@ public class SkillsIntegrationTests {
     }
 
     @Test
-    @Disabled
     @DisplayName("Return all skills given userId")
     public void testAPIReturnsSkillsGivenUser() {
         Response response = given()
