fac304a0767a45e75ea08d533bfef71e515fcebb
vikhyat187 <vikhyatbhatnagarh@gmail.com>
2023-12-07 07:10:01 +0530

    Added integration tests for /skills endpoint

--- skill-tree /src/test/java/com/RDS/skilltree/SkillsIntegrationTests.java
+++ skill-tree /src/test/java/com/RDS/skilltree/SkillsIntegrationTests.java
@@ -1,25 +1,82 @@
 package com.RDS.skilltree;
 
+import com.RDS.skilltree.Skill.*;
+import com.RDS.skilltree.User.*;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.UUID;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 @Testcontainers
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT )
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SkillsIntegrationTests {
-
+    private UserRepository userRepository;
+    private SkillRepository skillRepository;
     private static MySQLContainer mysqlContainer;
+    private final UserService userService;
+    private final SkillsService skillsService;
+    private UserDTO user;
+    private SkillDTO skill;
+
+
+    @Autowired
+    public SkillsIntegrationTests(UserService userService, UserRepository userRepository, SkillsService skillsService, SkillRepository skillRepository) {
+        this.userService = userService;
+        this.userRepository = userRepository;
+        this.skillsService = skillsService;
+        this.skillRepository = skillRepository;
+    }
+
+    @LocalServerPort
+    private int port;
+
+    @After
+    private void setup() {
+        RestAssured.baseURI = "http://localhost:";
+    }
+
+    @BeforeEach
+    private void addData() throws MalformedURLException {
+        user = userService.createUser(UserDRO.builder()
+                .role(UserRole.MEMBER)
+                .rdsUserId("p6Bo61VEClhtVdwW0ihg")
+                .lastName("Doe")
+                .firstName("John")
+                .imageUrl(new URL("https://res.cloudinary.com/realdevsquad/image/upload/v1666193594/profile/p6Bo61VEClhtVdwW0iGH/lezguwdq5bgzawa3.jpg"))
+                .build());
+
+        skill = skillsService.createSkill(
+                SkillDRO.builder()
+                        .name("Java")
+                        .type(SkillType.ATOMIC)
+                        .createdBy(user.getId())
+                        .build());
+    }
+
+    @AfterEach
+    private void cleanUp() {
+        skillRepository.deleteAll();
+        userRepository.deleteAll();
+    }
 
     static {
         mysqlContainer = new MySQLContainer("mysql:latest")
@@ -27,131 +84,217 @@ public class SkillsIntegrationTests {
                 .withUsername("root")
                 .withPassword("password");
         mysqlContainer.start();
-        RestAssured.baseURI = "http://localhost:8080";
     }
 
     @DynamicPropertySource
-    static void configureTestProperties(DynamicPropertyRegistry registry){
-        registry.add("spring.datasource.url", ()-> mysqlContainer.getJdbcUrl());
-        System.out.println("The spring datasource url is " + mysqlContainer.getJdbcUrl());
+    static void configureTestProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+        registry.add("spring.datasource.url", () -> mysqlContainer.getJdbcUrl());
         registry.add("spring.datasource.username", () -> mysqlContainer.getUsername());
         registry.add("spring.datasource.password", () -> mysqlContainer.getPassword());
-        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create");
     }
 
     @Test
     @DisplayName("Return all skills")
-    public void testAPIReturnsAllSkills() {
-
+    public void testAPIReturns200_OnAllSkillsFound() {
         Response response = given()
-                .get("/skills");
+                .port(port)
+                .get("/v1/skills/");
 
-        /*
-       {
-           "code": 200,
-           "data":
-                [
-                    {
-                    "id": "s1",
-                    "name": "Java",
-                    "type": "atomic / derived",
-                    "description": "string"
-                    }
-                ]
-            }
-
-         */
         response.then()
                 .statusCode(200)
-                .body("code",equalTo(200))
-                .body("data", hasSize(1))
-                .body("data[0].id", equalTo("s1"))
-                .body("data[0].name",equalTo("Java"))
-                .body("data[0].type", anyOf(equalTo("ATOMIC"), equalTo("DERIVED")))
-                .body("data[0].description",isA(String.class));
+                .body("content", hasSize(1))
+                .body("content[0].type", equalTo("ATOMIC"))
+                .body("content[0].name", equalTo("Java"))
+                .body("content[0].users", empty())
+                .body("totalPages", equalTo(1))
+                .body("totalElements", equalTo(1))
+                .body("last", equalTo(true))
+                .body("size", equalTo(100))
+                .body("number", equalTo(0))
+                .body("numberOfElements", equalTo(1))
+                .body("empty", equalTo(false));
     }
 
     @Test
-    @DisplayName("Return all skills given userId")
-    public void testAPIReturnsSkillsGivenUser() {
+    @DisplayName("Return all skills")
+    public void testAPIReturns200_OnNoSkillsFound() {
+        skillRepository.deleteAll();
         Response response = given()
-                .get("/skills/u1");
+                .port(port)
+                .get("/v1/skills/");
+
+        response.then()
+                .log().all()
+                .statusCode(200)
+                .body("content", hasSize(0))
+                .body("totalPages", equalTo(0))
+                .body("totalElements", equalTo(0))
+                .body("last", equalTo(true))
+                .body("size", equalTo(100))
+                .body("number", equalTo(0))
+                .body("numberOfElements", equalTo(0))
+                .body("empty", equalTo(true));
+    }
+
+    @Test
+    @DisplayName("Return skill given skillId")
+    public void testAPIReturns200_OnSkillFoundById() {
+        UUID skillId = skill.getId();
+
+        Response response = given()
+                .port(port)
+                .pathParam("id", skillId)
+                .get("/v1/skills/{id}");
 
         response.then()
                 .statusCode(200)
                 .contentType("application/json")
-                .body("code",equalTo(200))
-                .body("data", hasSize(1))
-                .body("data[0].id", equalTo("s1"))
-                .body("data[0].name",equalTo("Java"))
-                .body("data[0].type", anyOf(equalTo("ATOMIC"), equalTo("DERIVED")))
-                .body("data[0].description",isA(String.class));
+                .body("id", equalTo(String.valueOf(skillId)))
+                .body("name", equalTo("Java"))
+                .body("type", equalTo("ATOMIC"));
     }
 
     @Test
-    @Disabled
-    @DisplayName("Return 400 on incorrect userId")
-    public void testAPIReturns400_OnIncorrectUserId() {
+    @DisplayName("Return 404 on skill not found given SkillId")
+    public void testAPIReturns404_OnSkillNotFound() {
+        UUID skillId = UUID.randomUUID();
         Response response = given()
-                .get("/skills/{userId}");
-
-        response.then()
-                .statusCode(400)
-                .body("code",equalTo(400))
-                .body("message",equalTo("Invalid userId passed"));
-
-    }
-
-    @Test
-    @Disabled
-    @DisplayName("Return 404 on, no skills found on given userId")
-    public void testAPIReturns404_OnNoSkillsFound() {
-        Response response = given()
-                .get("/skills/{userId}");
+                .port(port)
+                .pathParam("id", skillId)
+                .get("/v1/skills/{id}");
 
         response.then()
                 .statusCode(404)
-                .body("code", equalTo(404))
-                .body("message",equalTo("No skills associated with the user"));
+                .body("message", equalTo("Skill not found with given Id"));
 
     }
 
     @Test
-    @Disabled
-    @DisplayName("Return users having skill given skill name")
-    public void testAPIReturns200_OnSkillGivenSkillName() {
+    @DisplayName("Return 200 on, skill with given name")
+    public void testAPIReturns200_OnSkillFoundGivenName() {
         Response response = given()
-                .get("/skills/{skillName}");
-
-        /*
-
-        [
-          {
-            "skillName": "Java",
-            "userName": "John doe"
-          }
-        ]
-         */
+                .port(port)
+                .pathParam("name", "Java")
+                .get("/v1/skills/name/{name}");
 
         response.then()
                 .statusCode(200)
                 .contentType("application/json")
-                .body("code",equalTo(200))
-                .body("data", hasSize(1))
-                .body("data[0].skillName",equalTo("Java"))
-                .body("data[0].userName",equalTo("John doe"));
+                .body("name", equalTo("Java"))
+                .body("type", equalTo("ATOMIC"));
     }
 
     @Test
-    @Disabled
-    @DisplayName("Return 400 on invalid skill name passed")
-    public void testAPIReturns400_OnIncorrectSkillName() {
+    @DisplayName("Return 404, if skill given skill name is not found")
+    public void testAPIReturns404_OnSkillGivenSkillNameNotFound() {
         Response response = given()
-                .get("/skills/{skillName}");
+                .port(port)
+                .pathParam("name", "Go")
+                .get("/v1/skills/name/{name}");
+
+
+        response.then()
+                .statusCode(404)
+                .body("message", equalTo("Skill not found with the given name"));
+    }
+
+    @Test
+    @DisplayName("Return 400, if createdBy is not passed for Skill creation")
+    public void testAPIReturns400_OnCreatedByNotPassedForSKillCreation() {
+        SkillDRO skillDRO = SkillDRO.builder()
+                .name("Go")
+                .type(SkillType.ATOMIC)
+                .build();
+
+        Response response = given()
+                .port(port)
+                .contentType("application/json")
+                .body(skillDRO)
+                .post("/v1/skills/");
 
         response.then()
                 .statusCode(400)
-                .body("code",equalTo(400))
-                .body("message",equalTo("Invalid skillName passed"));
+                .body("message", equalTo("CreatedBy, Type and Name are mandatory values, anyone cannot be null"));
+    }
+
+    @Test
+    @DisplayName("Return 400, if type is not passed for Skill creation")
+    public void testAPIReturns400_OnTypeNotPassedForSkillCreation() {
+        SkillDRO skillDRO = SkillDRO.builder()
+                .name("Go")
+                .createdBy(user.getId())
+                .build();
+
+        Response response = given()
+                .port(port)
+                .contentType("application/json")
+                .body(skillDRO)
+                .post("/v1/skills/");
+
+        response.then()
+                .statusCode(400)
+                .body("message", equalTo("CreatedBy, Type and Name are mandatory values, anyone cannot be null"));
+    }
+
+    @Test
+    @DisplayName("Return 400, if name is not passed for Skill creation")
+    public void testAPIReturns400_OnNameNotPassedForSkillCreation() {
+        SkillDRO skillDRO = SkillDRO.builder()
+                .type(SkillType.ATOMIC)
+                .createdBy(user.getId())
+                .build();
+
+        Response response = given()
+                .port(port)
+                .contentType("application/json")
+                .body(skillDRO)
+                .post("/v1/skills/");
+
+        response.then()
+                .statusCode(400)
+                .body("message", equalTo("CreatedBy, Type and Name are mandatory values, anyone cannot be null"));
+    }
+
+    @Test
+    @DisplayName("Return 409, if name is already used for Skill creation")
+    public void testAPIReturns400_OnNameAlreadyUsedForSkillCreation() {
+        SkillDRO skillDRO = SkillDRO.builder()
+                .type(SkillType.ATOMIC)
+                .name("Java")
+                .createdBy(user.getId())
+                .build();
+
+        Response response = given()
+                .port(port)
+                .contentType("application/json")
+                .body(skillDRO)
+                .post("/v1/skills/");
+
+        response.then()
+                .statusCode(409)
+                .body("message", equalTo("Cannot create entry for Skill as Skill name is duplicate"));
+    }
+
+    @Test
+    @DisplayName("Return 201, on successful Skill creation")
+    public void testAPIReturns201_OnSuccessfulSkillCreation() {
+        SkillDRO skillDRO = SkillDRO.builder()
+                .type(SkillType.ATOMIC)
+                .name("Go")
+                .createdBy(user.getId())
+                .build();
+
+        Response response = given()
+                .port(port)
+                .contentType("application/json")
+                .body(skillDRO)
+                .post("/v1/skills/");
+
+        response.then()
+                .statusCode(201)
+                .contentType("application/json")
+                .body("name", equalTo("Go"))
+                .body("type", equalTo("ATOMIC"));
     }
 }
