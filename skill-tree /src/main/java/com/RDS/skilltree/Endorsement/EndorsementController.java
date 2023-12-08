c8f798426e25f91fa843cbc6d978aeb11bf1cef7
Prakash <om.pcprakash@gmail.com>
2023-12-08 21:51:56 +0530

    feat: add endorsement controller

---
+++ skill-tree /src/main/java/com/RDS/skilltree/Endorsement/EndorsementController.java
@@ -0,0 +1,25 @@
+package com.RDS.skilltree.Endorsement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("${API_V1_PREFIX}/endorsement")
+public class EndorsementController {
+
+    private final EndorsementService endorsementService;
+
+    @Autowired
+    public EndorsementController(EndorsementService endorsementService) {
+        this.endorsementService = endorsementService;
+    }
+
+    @GetMapping()
+    public List<EndorsementModel> getEndorsements() {
+        return endorsementService.getEndorsements();
+    }
+}
