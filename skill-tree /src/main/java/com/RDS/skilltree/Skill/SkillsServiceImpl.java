2a4d04bae803e4ccb450c4d8c8f0cac456b1e7a4
vikhyat187 <vikhyatbhatnagarh@gmail.com>
2023-12-12 08:35:05 +0530

    Using the @AllArgs to reduce boilerplatecode

--- skill-tree /src/main/java/com/RDS/skilltree/Skill/SkillsServiceImpl.java
+++ skill-tree /src/main/java/com/RDS/skilltree/Skill/SkillsServiceImpl.java
@@ -2,6 +2,7 @@ package com.RDS.skilltree.Skill;
 
 import com.RDS.skilltree.User.UserModel;
 import com.RDS.skilltree.User.UserRepository;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -14,35 +15,31 @@ import java.util.UUID;
 
 @Service
 @Slf4j
-public class SkillsServiceImpl implements SkillsService{
+@AllArgsConstructor
+public class SkillsServiceImpl implements SkillsService {
     private final SkillRepository skillRepository;
     private final UserRepository userRepository;
-    public SkillsServiceImpl(SkillRepository skillRepository,
-                             UserRepository userRepository){
-        this.userRepository = userRepository;
-        this.skillRepository = skillRepository;
-    }
 
     @Override
-    public SkillDTO getSkillById(UUID id){
+    public SkillDTO getSkillById(UUID id) {
         Optional<SkillModel> skillModel = skillRepository.findById(id);
         return skillModel.map(SkillDTO::getSkillsWithUsers).orElse(null);
     }
 
     @Override
-    public SkillDTO  getSkillByName(String skillName){
+    public SkillDTO getSkillByName(String skillName) {
         Optional<SkillModel> skillModel = skillRepository.findByName(skillName);
         return skillModel.map(SkillDTO::getSkillsWithUsers).orElse(null);
     }
 
     @Override
-    public Page<SkillDTO> getAllSkills(Pageable pageable){
+    public Page<SkillDTO> getAllSkills(Pageable pageable) {
         Page<SkillModel> skillModels = skillRepository.findAll(pageable);
         return skillModels.map(SkillDTO::getSkillsWithUsers);
     }
 
     @Override
-    public SkillDTO createSkill(SkillDRO skillDRO){
+    public SkillDTO createSkill(SkillDRO skillDRO) {
         SkillModel newSkill = SkillDRO.toModel(skillDRO);
         newSkill.setCreatedAt(Instant.now());
         newSkill.setUpdatedAt(Instant.now());
@@ -51,7 +48,7 @@ public class SkillsServiceImpl implements SkillsService{
         newSkill.setCreatedBy(user);
         try {
             skillRepository.save(newSkill);
-        } catch(DataIntegrityViolationException ex){
+        } catch (DataIntegrityViolationException ex) {
             log.error("Error saving the skills object with name : {}, with exception :{}", skillDRO.getName(), ex.getMessage(), ex);
             throw ex;
         }
