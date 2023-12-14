8291aae36b610f0cbfb79e67f1c50df8e89bb5ad
Randhir Kumar Singh <97341921+heyrandhir@users.noreply.github.com>
2023-12-14 09:37:07 +0530

    convert the models to respective DTO's

--- skill-tree /src/main/java/com/RDS/skilltree/Endorsement/EndorsementDTO.java
+++ skill-tree /src/main/java/com/RDS/skilltree/Endorsement/EndorsementDTO.java
@@ -1,13 +1,11 @@
 package com.RDS.skilltree.Endorsement;
 
 import com.RDS.skilltree.EndorsementList.EndorsementListModel;
-import com.RDS.skilltree.Skill.SkillModel;
-import com.RDS.skilltree.User.UserModel;
+import com.RDS.skilltree.Skill.SkillDTO;
+import com.RDS.skilltree.User.UserDTO;
 import com.RDS.skilltree.utils.TrackedProperties;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
-
-import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,16 +14,16 @@ import java.util.UUID;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class EndorsementDTO extends TrackedProperties {
     private UUID id;
-    private UserModel user;
-    private SkillModel skill;
+    private UserDTO user;
+    private SkillDTO skill;
     private EndorsementStatus status;
     private List<EndorsementListModel> endorsersList;
 
     public static EndorsementDTO toDto(EndorsementModel endorsementModel) {
         EndorsementDTO endorsementDTO = EndorsementDTO.builder()
                 .id(endorsementModel.getId())
-                .user(endorsementModel.getUser())
-                .skill(endorsementModel.getSkill())
+                .user(UserDTO.toDTO(endorsementModel.getUser()))
+                .skill(SkillDTO.toDto(endorsementModel.getSkill()))
                 .status(endorsementModel.getStatus())
                 .endorsersList(endorsementModel.getEndorsersList())
                 .build();
