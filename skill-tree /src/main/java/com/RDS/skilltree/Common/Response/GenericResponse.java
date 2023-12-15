0c295cde1cc77fa6f72080fc121bacf5d1e9dad1
Randhir Kumar Singh <97341921+heyrandhir@users.noreply.github.com>
2023-12-15 06:17:00 +0530

    Adds @JsonInclude(JsonInclude.Include.NON_NULL) to improve response class by excluding null fields

--- skill-tree /src/main/java/com/RDS/skilltree/Common/Response/GenericResponse.java
+++ skill-tree /src/main/java/com/RDS/skilltree/Common/Response/GenericResponse.java
@@ -1,5 +1,6 @@
 package com.RDS.skilltree.Common.Response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class GenericResponse<T> {
     private T data;
     private String message;
