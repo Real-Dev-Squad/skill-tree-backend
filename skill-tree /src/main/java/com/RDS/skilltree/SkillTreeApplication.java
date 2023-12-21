80f99886d6b5655fd533d730bfe4efd21b336432
vikhyat187 <vikhyatbhatnagarh@gmail.com>
2023-12-21 21:53:09 +0530

    Added removed dependencies

--- skill-tree /src/main/java/com/RDS/skilltree/SkillTreeApplication.java
+++ skill-tree /src/main/java/com/RDS/skilltree/SkillTreeApplication.java
@@ -1,24 +1,10 @@
 package com.RDS.skilltree;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Skill Tree",
-                version = "1.0.0",
-                description = "Skill Tree"
-        ),
-        tags = @Tag(
-                name = "Skill Tree",
-                description = "Skill Tree"
-        )
-)
 public class SkillTreeApplication {
 
     public static void main(String[] args) {
