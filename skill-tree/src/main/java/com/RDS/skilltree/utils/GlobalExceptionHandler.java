07ed4ef86cde0c2bd32e6e8d2c1b45d17be18800
vikhyat187 <vikhyatbhatnagarh@gmail.com>
2023-12-25 20:36:27 +0530

    Updated the Http Response code and added logging for debugging purpose

--- skill-tree/src/main/java/com/RDS/skilltree/utils/GlobalExceptionHandler.java
+++ skill-tree/src/main/java/com/RDS/skilltree/utils/GlobalExceptionHandler.java
@@ -3,15 +3,18 @@ package com.RDS.skilltree.utils;
 import com.RDS.skilltree.Common.Response.GenericResponse;
 import com.RDS.skilltree.Exceptions.EntityAlreadyExistsException;
 import com.RDS.skilltree.Exceptions.NoEntityException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler({NoEntityException.class})
     public ResponseEntity<GenericResponse<Object>> handleNoEntityException(NoEntityException ex){
+        log.error("NoEntityException - Error : {}", ex.getMessage(), ex);
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(new GenericResponse<>(null, ex.getMessage()));
@@ -19,16 +22,18 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({EntityAlreadyExistsException.class})
     public ResponseEntity<GenericResponse<Object>> handleEntityAlreadyExistsException(EntityAlreadyExistsException ex){
+        log.error("EntityAlreadyExistsException - Error : {}", ex.getMessage(), ex);
         return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
+                .status(HttpStatus.CONFLICT)
                 .body(new GenericResponse<>(null, ex.getMessage()));
     }
 
     @ExceptionHandler({RuntimeException.class})
     public ResponseEntity<GenericResponse<Object>> handleRuntimException(RuntimeException ex){
+        log.error("Runtime Exception - Error : {}", ex.getMessage(), ex);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new GenericResponse<>(null, ex.getMessage()));
+                .body(new GenericResponse<>(null, "Something went wrong, please try communicating on `#wg-skill-tree discord` channel" ));
     }
 
 }
