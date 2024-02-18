6d217a78b32709b1508335d50a80e3568fcdaf13
bhtibrewal <bhtibrewal@gmail.com>
2024-02-18 23:32:43 +0530

    add cors policy and read cookies

--- skill-tree/src/main/java/com/RDS/skilltree/Config/SecurityConfig.java
+++ skill-tree/src/main/java/com/RDS/skilltree/Config/SecurityConfig.java
@@ -13,6 +13,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @EnableWebSecurity
 @Configuration
@@ -28,7 +34,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource( corsConfigurationSource()))
                 .authorizeHttpRequests(auth->auth
                         .requestMatchers("/**").authenticated())
                 .exceptionHandling(ex->ex.authenticationEntryPoint(authEntryPoint))
@@ -40,6 +46,17 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("https://*.realdevsquad.com", "http://localhost:[*]"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
     @Bean
     public JWTAuthenticationFilter jwtAuthenticationFilter(){
         return new JWTAuthenticationFilter(userService);
--- skill-tree/src/main/java/com/RDS/skilltree/Filters/JWTAuthenticationFilter.java
+++ skill-tree/src/main/java/com/RDS/skilltree/Filters/JWTAuthenticationFilter.java
@@ -7,25 +7,31 @@ import com.RDS.skilltree.utils.JWTUtils;
 import com.RDS.skilltree.utils.RDSUser.Response;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
 
 import javax.security.sasl.AuthenticationException;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
 
+    @Value("${cookieName}")
+    private String cookieName;
     @Autowired
     private JWTUtils jwtUtils;
 
@@ -83,6 +89,13 @@ if(SecurityContextHolder.getContext().getAuthentication() == null) {
     }
 
     private String getJWTFromRequest(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+
+        Cookie RDScookie = WebUtils.getCookie(request, cookieName);
+        if(RDScookie != null)
+            return RDScookie.getValue();
+
+
         String bearerToken = request.getHeader("Authorization");
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
             return bearerToken.substring(7);
