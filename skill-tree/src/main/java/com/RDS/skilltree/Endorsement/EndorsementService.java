be1b2bba7bd4282922c17adb2e673d7869a1d32d
akhil <akhilkhubchandani@gmail.com>
2024-03-15 00:21:30 +0530

    added boilerplate code and data, updated api-contract

--- skill-tree/src/main/java/com/RDS/skilltree/Endorsement/EndorsementService.java
+++ skill-tree/src/main/java/com/RDS/skilltree/Endorsement/EndorsementService.java
@@ -2,10 +2,14 @@ package com.RDS.skilltree.Endorsement;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
+import java.io.IOException;
 import java.util.UUID;
 
 public interface EndorsementService {
     EndorsementDTO getEndorsementById(UUID id);
     Page<EndorsementModel> getEndorsements(PageRequest pageRequest);
+
+    /* TODO:Dummy JSON code, needs to be changed as part of #103 */
+    Page<EndorsementModelFromJSON> getEndorsementsFromDummyData(PageRequest pageRequest,String skillID,String userID) throws IOException;
     EndorsementModel createEndorsement(EndorsementDRO endorsementDRO);
 }
