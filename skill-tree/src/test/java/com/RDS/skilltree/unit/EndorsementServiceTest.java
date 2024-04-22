833e4be9aedc835e547c6f013013247adf25d96d
Atifsid <atifsiddiqui35@gmail.com>
2024-04-22 20:04:36 +0530

    fix: switch to InsufficientAuthenticationException (401, Unauthorized) if user in not superuser

--- skill-tree/src/test/java/com/RDS/skilltree/unit/EndorsementServiceTest.java
+++ skill-tree/src/test/java/com/RDS/skilltree/unit/EndorsementServiceTest.java
@@ -30,6 +30,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -620,9 +621,9 @@ public class EndorsementServiceTest {
         UUID endorsementId = UUID.randomUUID();
         String status = EndorsementStatus.APPROVED.name();
 
-        AccessDeniedException exception =
+        InsufficientAuthenticationException exception =
                 assertThrows(
-                        AccessDeniedException.class,
+                        InsufficientAuthenticationException.class,
                         () -> endorsementService.updateEndorsementStatus(endorsementId.toString(), status));
         assertEquals("Unauthorized access", exception.getMessage());
         verify(endorsementRepository, never()).save(any(EndorsementModel.class));
