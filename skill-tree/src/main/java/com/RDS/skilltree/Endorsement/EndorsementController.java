22eece341eaf3d1ded51a7c36d96082403a8d03b
yash raj <yashraj0207@outlook.com>
2024-06-25 22:39:29 +0530

    remove unused code in endorsement controller

--- skill-tree/src/main/java/com/RDS/skilltree/Endorsement/EndorsementController.java
+++ skill-tree/src/main/java/com/RDS/skilltree/Endorsement/EndorsementController.java
@@ -25,29 +25,6 @@ public class EndorsementController {
     private final EndorsementRepository endorsementRepository;
     private final SkillRepository skillRepository;
 
-    //    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    //    public ResponseEntity<GenericResponse<EndorsementDTO>> getEndorsementById(
-    //            @PathVariable(value = "id", required = true) String id) {
-    //        try {
-    //            UUID uuid = UUID.fromString(id);
-    //            EndorsementDTO response = endorsementService.getEndorsementById(uuid);
-    //            return ResponseEntity.ok()
-    //                    .body(new GenericResponse<EndorsementDTO>(response, "Data retrieved
-    // successfully"));
-    //        } catch (IllegalArgumentException e) {
-    //            String message = "Invalid UUID: " + id;
-    //            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-    //                    .body(new GenericResponse<EndorsementDTO>(null, message));
-    //        } catch (EntityNotFoundException e) {
-    //            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-    //                    .body(new GenericResponse<EndorsementDTO>(null, e.getMessage()));
-    //        } catch (Exception e) {
-    //            String message = "Something went wrong. Please contact admin.";
-    //            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-    //                    .body(new GenericResponse<EndorsementDTO>(null, message));
-    //        }
-    //    }
-
     @PostMapping
     // TODO : add a check for when a endorsement is already created by a user for a particular skill.
     public ResponseEntity<GenericResponse<EndorsementModel>> postEndorsement(
--- skill-tree/src/main/java/com/RDS/skilltree/Skill/SkillsController.java
+++ skill-tree/src/main/java/com/RDS/skilltree/Skill/SkillsController.java
@@ -7,8 +7,11 @@ import com.RDS.skilltree.User.UserModel;
 import com.RDS.skilltree.User.UserRepository;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
+
 import java.util.List;
 import java.util.Optional;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -20,21 +23,13 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/v1/skills")
 public class SkillsController {
     private final UserRepository userRepository;
     private final SkillRepository skillRepository;
     private final EndorsementRepository endorsementRepository;
-
-    public SkillsController(
-            UserRepository userRepository,
-            SkillRepository skillRepository,
-            EndorsementRepository endorsementRepository) {
-        this.userRepository = userRepository;
-        this.skillRepository = skillRepository;
-        this.endorsementRepository = endorsementRepository;
-    }
-
+    
     @GetMapping
     public GenericResponse<List<Skill>> getAllSkills(@RequestParam(required = false) String name) {
         skillRepository.findByName(name);
