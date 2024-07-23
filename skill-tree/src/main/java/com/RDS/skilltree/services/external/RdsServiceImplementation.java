0323cf871df27115a3397f6c14efd17dc233096a
yash raj <yashraj0207@outlook.com>
2024-07-24 00:42:37 +0530

    add try catch block to getUserDetails

--- skill-tree/src/main/java/com/RDS/skilltree/services/external/RdsServiceImplementation.java
+++ skill-tree/src/main/java/com/RDS/skilltree/services/external/RdsServiceImplementation.java
@@ -2,13 +2,17 @@ package com.RDS.skilltree.services.external;
 
 import com.RDS.skilltree.viewmodels.RdsUserViewModel;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
 public class RdsServiceImplementation implements RdsService {
+    private static final Logger log = LoggerFactory.getLogger(RdsServiceImplementation.class);
     private final RestTemplate restTemplate;
 
     @Value("${rds.backendBaseUrl}")
@@ -17,6 +21,13 @@ public class RdsServiceImplementation implements RdsService {
     @Override
     public RdsUserViewModel getUserDetails(String id) {
         String url = rdsBackendBaseUrl + "/users?id=" + id;
-        return restTemplate.getForObject(url, RdsUserViewModel.class);
+
+        try {
+            return restTemplate.getForObject(url, RdsUserViewModel.class);
+        } catch (
+                RestClientException error) {
+            log.error("Error calling url: {}, Error : {}", url, error.getMessage());
+            throw new RuntimeException("Failed to communicate with RDS backend");
+        }
     }
 }
