f85ac8bdb092b4351f4027b03b2af96c77b67cc5
Shyam Vishwakarma <shyamvswkrma@gmail.com>
2024-10-21 23:25:51 +0530

    fix format violation

--- skill-tree/src/test/java/com/RDS/skilltree/skills/GetAllSkillsIntegrationTest.java
+++ skill-tree/src/test/java/com/RDS/skilltree/skills/GetAllSkillsIntegrationTest.java
@@ -1,44 +1,43 @@
 package com.RDS.skilltree.skills;
 
+import com.RDS.skilltree.enums.SkillTypeEnum;
 import com.RDS.skilltree.models.Skill;
 import com.RDS.skilltree.repositories.SkillRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.springframework.http.MediaType;
-import com.RDS.skilltree.enums.SkillTypeEnum;
 import com.RDS.skilltree.services.SkillService;
 import jakarta.servlet.http.Cookie;
+import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.Arrays;
-
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class GetAllSkillsIntegrationTest {
 
-    @Autowired
-    private SkillService skillService;
+    @Autowired private SkillService skillService;
 
-    @Autowired
-    private SkillRepository skillRepository;
+    @Autowired private SkillRepository skillRepository;
 
-    @Autowired
-    private MockMvc mockMvc;
+    @Autowired private MockMvc mockMvc;
 
     private Cookie authCookie;
 
     @BeforeEach
     public void setUp() {
-        authCookie = new Cookie("rds-session-v2-development", "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJzOXpRVW00WGJWRXo3eHpSa2FadiIsInJvbGUiOiJzdXBlcl91c2VyIiwiaWF0IjoxNzI4NjY0NjA2LCJleHAiOjE3MzEyNTY2MDZ9.EyOFKrVcbleuTjUGic3GzOzYRDoLU4IShyoboe0MHlvWFOAfU2pchpXLE4NcyvdGUZ_tvoUecHd4kUkR8MkhxnkRNU3HE7N-1c1tFeYXZL0KfScJE9YzDXAl113Hx3eZVvYbhNjNUttbDlH4s_kR6YABC3sdbLGKEiLfmp9VeAs");
+        authCookie =
+                new Cookie(
+                        "rds-session-v2-development",
+                        "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJzOXpRVW00WGJWRXo3eHpSa2FadiIsInJvbGUiOiJzdXBlcl91c2VyIiwiaWF0IjoxNzI4NjY0NjA2LCJleHAiOjE3MzEyNTY2MDZ9.EyOFKrVcbleuTjUGic3GzOzYRDoLU4IShyoboe0MHlvWFOAfU2pchpXLE4NcyvdGUZ_tvoUecHd4kUkR8MkhxnkRNU3HE7N-1c1tFeYXZL0KfScJE9YzDXAl113Hx3eZVvYbhNjNUttbDlH4s_kR6YABC3sdbLGKEiLfmp9VeAs");
 
         skillRepository.deleteAll();
         Skill skill1 = new Skill();
@@ -58,9 +57,11 @@ public class GetAllSkillsIntegrationTest {
     @DisplayName("happy flow - returns all skills that are in db")
     public void getAllSkillsHappyFlow() throws Exception {
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/v1/skills")
-                        .cookie(authCookie)
-                        .accept(MediaType.APPLICATION_JSON))
+        mockMvc
+                .perform(
+                        MockMvcRequestBuilders.get("/v1/skills")
+                                .cookie(authCookie)
+                                .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Java"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value("Springboot"))
@@ -72,9 +73,11 @@ public class GetAllSkillsIntegrationTest {
     public void noSkillsAvailable_shouldReturnEmptyList() throws Exception {
         skillRepository.deleteAll();
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/v1/skills")
-                        .cookie(authCookie)
-                        .accept(MediaType.APPLICATION_JSON))
+        mockMvc
+                .perform(
+                        MockMvcRequestBuilders.get("/v1/skills")
+                                .cookie(authCookie)
+                                .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$").isEmpty());
     }
@@ -82,9 +85,11 @@ public class GetAllSkillsIntegrationTest {
     @Test
     @DisplayName("if invalid cookie, return 401")
     public void ifInvalidCoolie_returnUnauthorized() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/v1/skills")
-                        .cookie(new Cookie("cookie1", "eyJhbGciOiJSUz.eyJhbGciOiJSUz.EyJhbGciOiJSUz"))
-                        .accept(MediaType.APPLICATION_JSON))
+        mockMvc
+                .perform(
+                        MockMvcRequestBuilders.get("/v1/skills")
+                                .cookie(new Cookie("cookie1", "eyJhbGciOiJSUz.eyJhbGciOiJSUz.EyJhbGciOiJSUz"))
+                                .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isUnauthorized());
     }
 }
