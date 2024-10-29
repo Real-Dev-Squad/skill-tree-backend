73d1e227dce29a9a4e4d792d55bf8211926d1c35
raj1802 <dheeraj18200@gmail.com>
2024-10-29 11:40:18 +0530

    Fix formatting issues in unitTests

--- skill-tree/src/test/java/com/RDS/skilltree/unitTests/EndorsementsApiUnitTests.java
+++ skill-tree/src/test/java/com/RDS/skilltree/unitTests/EndorsementsApiUnitTests.java
@@ -1,5 +1,8 @@
 package com.RDS.skilltree.unitTests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 import com.RDS.skilltree.dtos.RdsGetUserDetailsResDto;
 import com.RDS.skilltree.models.Endorsement;
 import com.RDS.skilltree.models.Skill;
@@ -8,51 +11,41 @@ import com.RDS.skilltree.services.EndorsementServiceImplementation;
 import com.RDS.skilltree.services.external.RdsService;
 import com.RDS.skilltree.viewmodels.EndorsementViewModel;
 import com.RDS.skilltree.viewmodels.RdsUserViewModel;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import java.util.ArrayList;
-import java.util.List;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
-
 
 public class EndorsementsApiUnitTests {
 
-    @Mock
-    private EndorsementRepository endorsementRepository;
+    @Mock private EndorsementRepository endorsementRepository;
 
-    @Mock
-    private RdsService rdsService;
+    @Mock private RdsService rdsService;
 
-    @InjectMocks
-    private EndorsementServiceImplementation endorsementService;
+    @InjectMocks private EndorsementServiceImplementation endorsementService;
 
     @BeforeEach
     public void setup() {
-        MockitoAnnotations.openMocks(this);  // Initialize mocks
-
+        MockitoAnnotations.openMocks(this); // Initialize mocks
     }
-
 
     @Test
     @DisplayName("To test getAllEndorsementsBySkillId() method")
-    public void testGetAllEndorsementsBySkillId()
-    {
+    public void testGetAllEndorsementsBySkillId() {
         List<Endorsement> endorsements = new ArrayList<>();
 
-        Integer skillId =1;
+        Integer skillId = 1;
 
-        //initializing a skill
+        // initializing a skill
         Skill skill1 = new Skill();
         skill1.setId(skillId);
         skill1.setName("python");
 
-
-        //initializing an endorsement
+        // initializing an endorsement
         Endorsement endorsement1 = new Endorsement();
         endorsement1.setId(1);
         endorsement1.setSkill(skill1);
@@ -63,35 +56,30 @@ public class EndorsementsApiUnitTests {
         endorsements.add(endorsement1);
 
         RdsGetUserDetailsResDto rdsGetUserDetailsResDto1 = new RdsGetUserDetailsResDto();
-        RdsUserViewModel rdsUserViewModel1= new RdsUserViewModel();
+        RdsUserViewModel rdsUserViewModel1 = new RdsUserViewModel();
         rdsUserViewModel1.setId("123");
         rdsGetUserDetailsResDto1.setUser(rdsUserViewModel1);
 
-
         RdsGetUserDetailsResDto rdsGetUserDetailsResDto2 = new RdsGetUserDetailsResDto();
-        RdsUserViewModel rdsUserViewModel2= new RdsUserViewModel();
+        RdsUserViewModel rdsUserViewModel2 = new RdsUserViewModel();
         rdsUserViewModel2.setId("456");
         rdsGetUserDetailsResDto2.setUser(rdsUserViewModel2);
 
-
-
-        //Mock behaviour
+        // Mock behaviour
         when(endorsementRepository.findBySkillId(skillId)).thenReturn(endorsements);
-        when(rdsService.getUserDetails(endorsement1.getEndorseId())).thenReturn(rdsGetUserDetailsResDto1);
-        when(rdsService.getUserDetails(endorsement1.getEndorserId())).thenReturn(rdsGetUserDetailsResDto2);
+        when(rdsService.getUserDetails(endorsement1.getEndorseId()))
+                .thenReturn(rdsGetUserDetailsResDto1);
+        when(rdsService.getUserDetails(endorsement1.getEndorserId()))
+                .thenReturn(rdsGetUserDetailsResDto2);
 
+        // Act
 
-        //Act
+        List<EndorsementViewModel> endorsmentsBySkillId =
+                endorsementService.getAllEndorsementsBySkillId(skillId);
 
-        List<EndorsementViewModel> endorsmentsBySkillId = endorsementService.getAllEndorsementsBySkillId(skillId);
-
-        assert(endorsmentsBySkillId.size() == 1);
+        assert (endorsmentsBySkillId.size() == 1);
         assertEquals("456", endorsmentsBySkillId.get(0).getEndorser().getId());
         assertEquals("123", endorsmentsBySkillId.get(0).getEndorse().getId());
         assertEquals("python", endorsmentsBySkillId.get(0).getSkill().getName());
-
-
     }
-
-
 }
--- skill-tree/src/test/java/com/RDS/skilltree/unitTests/SkillsApiUnitTests.java
+++ skill-tree/src/test/java/com/RDS/skilltree/unitTests/SkillsApiUnitTests.java
@@ -1,20 +1,19 @@
 package com.RDS.skilltree.unitTests;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import com.RDS.skilltree.dtos.RdsGetUserDetailsResDto;
-import com.RDS.skilltree.dtos.SkillRequestsDto;
 import com.RDS.skilltree.enums.SkillTypeEnum;
 import com.RDS.skilltree.enums.UserRoleEnum;
 import com.RDS.skilltree.enums.UserSkillStatusEnum;
 import com.RDS.skilltree.exceptions.NoEntityException;
 import com.RDS.skilltree.exceptions.SkillAlreadyExistsException;
-import com.RDS.skilltree.models.Endorsement;
 import com.RDS.skilltree.models.JwtUser;
 import com.RDS.skilltree.models.Skill;
 import com.RDS.skilltree.models.UserSkills;
-import com.RDS.skilltree.repositories.EndorsementRepository;
 import com.RDS.skilltree.repositories.SkillRepository;
 import com.RDS.skilltree.repositories.UserSkillRepository;
-import com.RDS.skilltree.services.SkillService;
 import com.RDS.skilltree.services.SkillServiceImplementation;
 import com.RDS.skilltree.services.external.RdsService;
 import com.RDS.skilltree.utils.GenericResponse;
@@ -22,49 +21,37 @@ import com.RDS.skilltree.viewmodels.CreateSkillViewModel;
 import com.RDS.skilltree.viewmodels.RdsUserViewModel;
 import com.RDS.skilltree.viewmodels.SkillRequestsWithUserDetailsViewModel;
 import com.RDS.skilltree.viewmodels.SkillViewModel;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
 public class SkillsApiUnitTests {
 
-    @Mock
-    private SkillRepository skillRepository;
+    @Mock private SkillRepository skillRepository;
 
-    @Mock
-    private UserSkillRepository userSkillRepository;
+    @Mock private UserSkillRepository userSkillRepository;
 
+    @Mock private RdsService rdsService;
 
+    @Mock private Authentication authentication;
 
-    @Mock
-    private RdsService rdsService;
-
-    @Mock
-    private Authentication authentication;
-
-    @Mock
-    private SkillRequestsWithUserDetailsViewModel skillRequestsWithUserDetailsViewModel;
+    @Mock private SkillRequestsWithUserDetailsViewModel skillRequestsWithUserDetailsViewModel;
 
     @InjectMocks
     private SkillServiceImplementation skillService; // Class containing the getAll() method
 
     @BeforeEach
     public void setup() {
-        MockitoAnnotations.openMocks(this);  // Initialize mocks
+        MockitoAnnotations.openMocks(this); // Initialize mocks
 
         SecurityContext securityContext = mock(SecurityContext.class);
         SecurityContextHolder.setContext(securityContext);
@@ -72,9 +59,9 @@ public class SkillsApiUnitTests {
     }
 
     @Test
-    @DisplayName("Test the getAll() method returns all the created skills, where the skills return in a sorted order based on skill-Id.")
-    public void getAllSkillsHappyFlow()
-    {
+    @DisplayName(
+            "Test the getAll() method returns all the created skills, where the skills return in a sorted order based on skill-Id.")
+    public void getAllSkillsHappyFlow() {
 
         Skill skill1 = new Skill();
         skill1.setId(1);
@@ -85,7 +72,6 @@ public class SkillsApiUnitTests {
         skill2.setId(2);
         skill2.setName("Spring Boot");
         skill2.setType(SkillTypeEnum.ATOMIC);
-
 
         // Mock the repository's behavior
         when(skillRepository.findAll()).thenReturn(Arrays.asList(skill1, skill2));
@@ -102,7 +88,6 @@ public class SkillsApiUnitTests {
         verify(skillRepository, times(1)).findAll();
     }
 
-
     @Test
     @DisplayName("Test the create() skill method, which returns the created skill data")
     public void testCreateSkill_Success() {
@@ -114,14 +99,11 @@ public class SkillsApiUnitTests {
         // Mock JwtUser with rdsUserId and role
         JwtUser jwtUser = new JwtUser("123", UserRoleEnum.USER);
 
-
-
         RdsUserViewModel viewModel = new RdsUserViewModel();
         viewModel.setId("123");
 
         RdsGetUserDetailsResDto userDetails = new RdsGetUserDetailsResDto();
         userDetails.setUser(viewModel);
-
 
         Skill skillEntity = new Skill();
         skillEntity.setId(5);
@@ -133,17 +115,12 @@ public class SkillsApiUnitTests {
 
         // Mock behavior
 
-        when(authentication.getPrincipal()).thenReturn(jwtUser);// Return mock JwtUser
-        when(rdsService.getUserDetails(jwtUser.getRdsUserId())).thenReturn(userDetails);  // Use rdsUserId
+        when(authentication.getPrincipal()).thenReturn(jwtUser); // Return mock JwtUser
+        when(rdsService.getUserDetails(jwtUser.getRdsUserId()))
+                .thenReturn(userDetails); // Use rdsUserId
         when(skillRepository.saveAndFlush(any(Skill.class))).thenReturn(skillEntity);
 
-
-
-
-
         when(skillRepository.existsByName("python")).thenReturn(false);
-
-
 
         // Act: Call the method
         SkillViewModel actualViewModel = skillService.create(createSkill);
@@ -157,9 +134,7 @@ public class SkillsApiUnitTests {
         // Verify that the repository and service were called correctly
         verify(skillRepository, times(1)).existsByName("python");
         verify(skillRepository, times(1)).saveAndFlush(any(Skill.class));
-
     }
-
 
     @Test
     @DisplayName("Test the create() skill method, when an already present skill is being added")
@@ -173,11 +148,11 @@ public class SkillsApiUnitTests {
         when(skillRepository.existsByName("python")).thenReturn(true);
 
         // Act & Assert: Verify that exception is thrown
-        SkillAlreadyExistsException exception = assertThrows(
-                SkillAlreadyExistsException.class,
-                () -> skillService.create(createSkill),
-                "Skill with name "+createSkill.getName()+"already exists"
-        );
+        SkillAlreadyExistsException exception =
+                assertThrows(
+                        SkillAlreadyExistsException.class,
+                        () -> skillService.create(createSkill),
+                        "Skill with name " + createSkill.getName() + "already exists");
 
         assertEquals("Skill with name python already exists", exception.getMessage());
 
@@ -186,44 +161,41 @@ public class SkillsApiUnitTests {
         verify(skillRepository, never()).saveAndFlush(any(Skill.class));
     }
 
-    //To be modified
+    // To be modified
 
+    @Test
+    @DisplayName("To Test the approveRejectSkillRequest() method, when action is approved")
+    public void testApproveRejectSkillRequest() {
 
+        // Arrange
+        Integer skillId = 1;
+        String endorseId = "123";
+        UserSkillStatusEnum action = UserSkillStatusEnum.APPROVED;
 
-@Test
-@DisplayName("To Test the approveRejectSkillRequest() method, when action is approved")
-public void testApproveRejectSkillRequest(){
+        Skill skill1 = new Skill();
+        skill1.setId(1);
 
-    // Arrange
-    Integer skillId = 1;
-    String endorseId = "123";
-    UserSkillStatusEnum action = UserSkillStatusEnum.APPROVED;
+        List<UserSkills> existingSkillRequest = new ArrayList<>();
+        UserSkills userSkill1 = new UserSkills();
+        userSkill1.setId("1");
+        userSkill1.setUserId("123");
+        userSkill1.setStatus(UserSkillStatusEnum.PENDING);
+        userSkill1.setSkill(skill1);
+        existingSkillRequest.add(userSkill1);
 
-     Skill skill1 = new Skill();
-     skill1.setId(1);
+        when(userSkillRepository.findByUserIdAndSkillId(userSkill1.getUserId(), skill1.getId()))
+                .thenReturn(existingSkillRequest);
+        when(userSkillRepository.save(userSkill1)).thenReturn(userSkill1);
 
-    List<UserSkills> existingSkillRequest = new ArrayList<>();
-    UserSkills userSkill1 = new UserSkills();
-    userSkill1.setId("1");
-    userSkill1.setUserId("123");
-    userSkill1.setStatus(UserSkillStatusEnum.PENDING);
-    userSkill1.setSkill(skill1);
-    existingSkillRequest.add(userSkill1);
+        // Act: Call the method
+        GenericResponse<String> response =
+                skillService.approveRejectSkillRequest(skillId, endorseId, action);
 
-    when(userSkillRepository.findByUserIdAndSkillId(userSkill1.getUserId(), skill1.getId())).thenReturn(existingSkillRequest);
-    when(userSkillRepository.save(userSkill1)).thenReturn(userSkill1);
-
-
-    // Act: Call the method
-    GenericResponse<String> response=  skillService.approveRejectSkillRequest(skillId,endorseId,action);
-
-// Assert
-    assertNotNull(response);
-    assertEquals("approved", response.getMessage()); // Depending on the action
-    assertEquals(UserSkillStatusEnum.APPROVED, userSkill1.getStatus());
-
-
-}
+        // Assert
+        assertNotNull(response);
+        assertEquals("approved", response.getMessage()); // Depending on the action
+        assertEquals(UserSkillStatusEnum.APPROVED, userSkill1.getStatus());
+    }
 
     @Test
     @DisplayName("To Test the approveRejectSkillRequest() method, when action is rejected")
@@ -245,11 +217,13 @@ public void testApproveRejectSkillRequest(){
         existingSkillRequest.add(mockSkillRequest);
 
         // Mock repository behavior
-        when(userSkillRepository.findByUserIdAndSkillId(endorseId, skillId)).thenReturn(existingSkillRequest);
+        when(userSkillRepository.findByUserIdAndSkillId(endorseId, skillId))
+                .thenReturn(existingSkillRequest);
         when(userSkillRepository.save(mockSkillRequest)).thenReturn(mockSkillRequest);
 
         // Act
-        GenericResponse<String> response = skillService.approveRejectSkillRequest(skillId, endorseId, action);
+        GenericResponse<String> response =
+                skillService.approveRejectSkillRequest(skillId, endorseId, action);
 
         // Assert
         assertNotNull(response);
@@ -269,21 +243,17 @@ public void testApproveRejectSkillRequest(){
         UserSkillStatusEnum action = UserSkillStatusEnum.APPROVED;
 
         // Mock repository behavior for no entity found
-        when(userSkillRepository.findByUserIdAndSkillId(endorseId, skillId)).thenReturn(new ArrayList<>());
+        when(userSkillRepository.findByUserIdAndSkillId(endorseId, skillId))
+                .thenReturn(new ArrayList<>());
 
         // Act & Assert
-        assertThrows(NoEntityException.class, () -> {
-            skillService.approveRejectSkillRequest(skillId, endorseId, action);
-        });
+        assertThrows(
+                NoEntityException.class,
+                () -> {
+                    skillService.approveRejectSkillRequest(skillId, endorseId, action);
+                });
 
         // Verify that the save method was never called
         verify(userSkillRepository, never()).save(any(UserSkills.class));
     }
-
-
-
-
-
-
-
 }
