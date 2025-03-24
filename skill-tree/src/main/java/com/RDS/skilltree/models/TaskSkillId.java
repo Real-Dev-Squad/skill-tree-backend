79c9765e143450282565c79d886ec1c14112df25
Sunil Kumar <sunilkumar24.dev@gmail.com>
2025-03-24 19:29:20 +0500

    add @NotNull validation and serialVersionUID to TaskSkillID model

--- skill-tree/src/main/java/com/RDS/skilltree/models/TaskSkillId.java
+++ skill-tree/src/main/java/com/RDS/skilltree/models/TaskSkillId.java
@@ -1,6 +1,8 @@
 package com.RDS.skilltree.models;
 
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotNull;
+import java.io.Serial;
 import java.io.Serializable;
 import lombok.*;
 
@@ -12,6 +14,11 @@ import lombok.*;
 @Builder
 @EqualsAndHashCode
 public class TaskSkillId implements Serializable {
+    @Serial private static final long serialVersionUID = 1L;
+
+    @NotNull(message = "Task ID cannot be null")
     private String taskId;
+
+    @NotNull(message = "Skill ID cannot be null")
     private Integer skillId;
 }
