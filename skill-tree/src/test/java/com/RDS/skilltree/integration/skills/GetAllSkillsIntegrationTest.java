e022fedfb8194cc5e0c629f032a5b098389ceb5c
Shyam Vishwakarma <shyamvswkrma@gmail.com>
2025-04-23 09:43:06 +0530

    fix: remove harcoded value from test container

--- skill-tree/src/test/java/com/RDS/skilltree/integration/skills/GetAllSkillsIntegrationTest.java
+++ skill-tree/src/test/java/com/RDS/skilltree/integration/skills/GetAllSkillsIntegrationTest.java
@@ -22,6 +22,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -33,7 +34,8 @@ import utils.WithCustomMockUser;
 @ActiveProfiles("test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class GetAllSkillsIntegrationTest extends TestContainerManager {
+@Import(TestContainerManager.class)
+public class GetAllSkillsIntegrationTest {
 
     @Autowired private SkillService skillService;
 
