fab2c173fc573dcf557d08738d0be72cbfd8adc9
Shyam Vishwakarma <144812100+Shyam-Vishwakarma@users.noreply.github.com>
2025-05-07 00:52:05 +0530

    test: integration test for GET /v1/skills/{skillId}/endorsements (#194)
    * fix: remove harcoded value from test container
    
    * fix: add integration tests for GET /v1/skills/{id}/endorsements
    
    * fix: refactor the code, remove unused dependence, redundant comments

--- skill-tree/src/test/java/com/RDS/skilltree/integration/skills/GetAllSkillRequestIntegrationTest.java
+++ skill-tree/src/test/java/com/RDS/skilltree/integration/skills/GetAllSkillRequestIntegrationTest.java
@@ -27,6 +27,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -39,7 +40,8 @@ import utils.WithCustomMockUser;
 @ActiveProfiles("test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class GetAllSkillRequestIntegrationTest extends TestContainerManager {
+@Import(TestContainerManager.class)
+public class GetAllSkillRequestIntegrationTest {
     @Autowired private MockMvc mockMvc;
 
     @Autowired private UserSkillRepository userSkillRepository;
