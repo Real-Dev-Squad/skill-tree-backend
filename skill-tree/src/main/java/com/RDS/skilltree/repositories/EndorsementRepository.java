d61cc9109d3ed11d711f2dcf91f4c84d3b3dd79e
Shyam Vishwakarma <shyamvswkrma@gmail.com>
2025-05-10 23:33:21 +0530

    fix: update query formatting

--- skill-tree/src/main/java/com/RDS/skilltree/repositories/EndorsementRepository.java
+++ skill-tree/src/main/java/com/RDS/skilltree/repositories/EndorsementRepository.java
@@ -11,10 +11,12 @@ public interface EndorsementRepository extends JpaRepository<Endorsement, Intege
 
     List<Endorsement> findByEndorseIdAndSkillId(String endorseId, Integer skillId);
 
-    @Query(
-            "SELECT EXISTS"
-                    + "(SELECT 1 FROM Endorsement e JOIN UserSkills us ON (e.skill.id = us.skill.id and e.endorseId = us.userId)"
-                    + "WHERE (e.endorserId = :endorserId and us.userId = :endorseId and us.skill.id = :skillId))")
+    @Query("""
+            SELECT EXISTS
+            (SELECT 1
+            FROM Endorsement e JOIN UserSkills us ON (e.skill.id = us.skill.id and e.endorseId = us.userId)
+            WHERE (e.endorserId = :endorserId and us.userId = :endorseId and us.skill.id = :skillId))
+            """)
     boolean existsByEndorseIdAndEndorserIdAndSkillId(
             @Param("endorseId") String endorseId,
             @Param("endorserId") String endorserId,
