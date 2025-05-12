8e26af329e03198181a9fcb6536b41296949f1e0
Shyam Vishwakarma <shyamvswkrma@gmail.com>
2025-05-12 11:39:18 +0530

    fix: update query to scan only endorsement table, as joining user-skills is redundant

--- skill-tree/src/main/java/com/RDS/skilltree/repositories/EndorsementRepository.java
+++ skill-tree/src/main/java/com/RDS/skilltree/repositories/EndorsementRepository.java
@@ -12,11 +12,13 @@ public interface EndorsementRepository extends JpaRepository<Endorsement, Intege
     List<Endorsement> findByEndorseIdAndSkillId(String endorseId, Integer skillId);
 
     @Query("""
-            SELECT EXISTS
-            (SELECT 1
-            FROM Endorsement e JOIN UserSkills us ON (e.skill.id = us.skill.id and e.endorseId = us.userId)
-            WHERE (e.endorserId = :endorserId and us.userId = :endorseId and us.skill.id = :skillId))
-            """)
+            SELECT EXISTS (
+                SELECT 1
+                FROM Endorsement e
+                WHERE e.endorserId = :endorserId
+                  AND e.endorseId = :endorseId
+                  AND e.skill.id = :skillId
+            )""")
     boolean existsByEndorseIdAndEndorserIdAndSkillId(
             @Param("endorseId") String endorseId,
             @Param("endorserId") String endorserId,
