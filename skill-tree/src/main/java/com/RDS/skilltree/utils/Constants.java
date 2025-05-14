ee11286358399c375b2eabb887e4e5de6cd22200
Shyam Vishwakarma <shyamvswkrma@gmail.com>
2025-05-14 22:09:43 +0530

    add class to contain constant messages across app

---
+++ skill-tree/src/main/java/com/RDS/skilltree/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.RDS.skilltree.utils;
+
+public class Constants {
+    // Exception messages
+    public static final String SELF_ENDORSEMENT_NOT_ALLOWED = "Self endorsement not allowed";
+    public static final String SKILL_NOT_FOUND = "Skill does not exist";
+    public static final String ENDORSEMENT_ALREADY_EXISTS = "Endorsement already exists";
+    public static final String ENDORSEMENT_NOT_FOUND = "Endorsement not found";
+}
