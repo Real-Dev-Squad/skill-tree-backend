9706a09648ae6fe434872305c2d347e51e079a18
Shyam Vishwakarma <shyamvswkrma@gmail.com>
2025-05-23 01:51:35 +0530

    refactor: add exception messages in constants and replace its usage

--- skill-tree/src/main/java/com/RDS/skilltree/utils/Constants.java
+++ skill-tree/src/main/java/com/RDS/skilltree/utils/Constants.java
@@ -8,5 +8,9 @@ public class Constants {
         public static final String SKILL_NOT_FOUND = "Skill does not exist";
         public static final String ENDORSEMENT_ALREADY_EXISTS = "Endorsement already exists";
         public static final String ENDORSEMENT_NOT_FOUND = "Endorsement not found";
+        public static final String ENDORSEMENT_MESSAGE_EMPTY = "Endorsement message cannot be empty";
+        public static final String USER_NOT_FOUND = "Error getting user details";
+        public static final String UNAUTHORIZED_ENDORSEMENT_UPDATE =
+                "Not authorized to update this endorsement";
     }
 }
