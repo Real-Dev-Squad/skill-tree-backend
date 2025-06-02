526e35ab287bb73690bc081cf1d90c44b971fe05
Shyam Vishwakarma <144812100+Shyam-Vishwakarma@users.noreply.github.com>
2025-06-02 20:09:56 +0530

    Merge pull request #207 from Shyam-Vishwakarma/integration-tests
    test: add integration tests for PATCH v1/endorsements/{id} endpoint

--- skill-tree/src/main/java/com/RDS/skilltree/utils/Constants.java
+++ skill-tree/src/main/java/com/RDS/skilltree/utils/Constants.java
@@ -8,5 +8,12 @@ public class Constants {
         public static final String SKILL_NOT_FOUND = "Skill does not exist";
         public static final String ENDORSEMENT_ALREADY_EXISTS = "Endorsement already exists";
         public static final String ENDORSEMENT_NOT_FOUND = "Endorsement not found";
+        public static final String ENDORSEMENT_MESSAGE_EMPTY = "Endorsement message cannot be empty";
+        public static final String USER_NOT_FOUND = "Error getting user details";
+        public static final String UNAUTHORIZED_ENDORSEMENT_UPDATE =
+                "Not authorized to update this endorsement";
+        public static final String INVALID_ACCESS_TOKEN =
+                "The access token provided is expired, revoked, malformed, or invalid for other reasons.";
+        public static final String ACCESS_DENIED = "Access Denied";
     }
 }
